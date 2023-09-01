ed8db1a1444fcfd056b177216b2a83751c395eff
Shridhar Sarraf <shridharsarraf998@gmail.com>
2023-09-01 12:38:46 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 
 import ProductListing from '@/app/components/productListing'
 import Image from 'next/image'
-
+import { CartProvider } from '../context/CartContext';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <CartProvider>
+      <main className="flex min-h-screen flex-col items-center justify-between">
       <ProductListing/>
     </main>
+    </CartProvider>
   )
 }
