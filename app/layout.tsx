0fa9287076873cd869c2b4740f0d0adcb37f5761
Shridhar Sarraf <shridharsarraf998@gmail.com>
2023-09-01 12:52:19 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-
+import { CartProvider } from '../context/CartContext';
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -18,9 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        
+        <CartProvider>
            {children}
-        
+        </CartProvider>
       </body>
     </html>
   )
